25cf7155201442aea323285b31260ed712cdc4d0
Yunus Olgan <yunusolgan@gmail.com>
2022-03-25 15:52:16 +0300

    ...

--- src/components/pages/Candidates.js
+++ src/components/pages/Candidates.js
@@ -1,20 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux'
-import { connect } from "react-redux";
 import { fetchUser } from "../../actions/userActions";
 import UserList from "../UserList";
 
-
 export default function Candidates  ()  {
-  // useEffect(() => {
-  //   props.fetchUser();
-  // }, []);
+ 
 const dispatch = useDispatch()
+const state = useSelector(state => state)
 useEffect(() => {
   dispatch(fetchUser())
 }, [])
-const state = useSelector(state => state)
-console.log(state)
+
   return (
     <div>
       <UserList list={state.userReducer} />
@@ -22,8 +18,4 @@ console.log(state)
   );
 };
 
-// const mapStateToProps = ({ userReducer }) => ({ userReducer });
 
-// const mapDispatchToProps = { fetchUser };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Candidates);
